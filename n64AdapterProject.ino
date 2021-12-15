67990b0c8194c15af5a7fd010a9348f8ce4ca89c
benlagreca02 <benlagreca02@gmail.com>
2021-12-15 13:15:32 -0500

    starting real work

--- n64AdapterProject.ino
+++ n64AdapterProject.ino
@@ -1,11 +1,28 @@
-#define THE_GREATEST_PROGRAMMER "terry davis"
+
+
+
+/*
+*	DATA ENCODING
+*	us| 0 1 2 3
+*   --------------
+*	 0| 0 0 0 1
+*	 1| 0 1 1 1
+*
+*  POLL: 2_0 0000 0011
+*  ANSR: 32bits + 1 end bit
+*
+*/
+
 
 void setup() {
   // put your setup code here, to run once:
 
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+	// SEND OUT THE REQUEST
 
 }
