5de46b55d7da0c29482744e27431ac6bfbc0c8de
Ben LaGreca <benlagreca02@gmail.com>
2022-07-19 21:15:07 -0400

    properly interpreting joystick data, but data seems bad

--- joystickEmulator/joystickEmulator.ino
+++ joystickEmulator/joystickEmulator.ino
@@ -10,7 +10,9 @@
 #include <Joystick.h>
 #include <Wire.h>
 #include "buttonMasks.h"
-#define NUM_BYTES_EXPECTED 4  // we receive 4 bytes (32 bits)
+#define NUM_BYTES_EXPECTED 4
+
+// global joystick object, needs to be used in setup and the i2cRecv. handler so global
 Joystick_ usrJoystick;
 
 #define AXIS_MIN  0x00
@@ -30,31 +32,40 @@ void setup(){
     
 }
 
-// almost like an interrupt routine
+// almost like an interrupt routine 
 void i2cRecieveHandler(int numBytes){
     Serial.print("I2C MSG RECIEVED:  ");
-    uint8_t numBytesRecieved = 0;
+    byte numBytesRecieved = 0;
     byte dataRecieved[NUM_BYTES_EXPECTED];
     
     while(Wire.available()){
         dataRecieved[numBytesRecieved] = Wire.read();
         numBytesRecieved++;
     }
+
+
+    // Temporary test, byte 0
+    byte rawX = dataRecieved[2];
+    int joyValX = (bitRead(rawX, 7) == 1) ? -dataRecieved[2] : dataRecieved[2];
+
+    byte rawY = dataRecieved[3];
+    int joyValY = (bitRead(rawY, 7) == 1) ? -dataRecieved[3] : dataRecieved[3];
+    
     
     Serial.print(dataRecieved[0], BIN);
     Serial.print(" | ");
     Serial.print(dataRecieved[1], BIN);
     Serial.print(" | ");
-    Serial.print(dataRecieved[2], HEX);
+    Serial.print(joyValX, DEC);
     Serial.print(" | ");
-    Serial.println(dataRecieved[3], HEX);
+    Serial.println(joyValY, DEC);
 
     // L, R, and C buttons are stored in the SECOND byte
     // A,B,Z,START, and D pad are stored in FIRST byte
     // Joystick X axis is stored in THIRD byte
     // Joystick Y axis is stored in FOURTH byte
     
-    // A button is 0th bit of 0th byte
+    // A button is 0th bit of 0th byte etc... 
     usrJoystick.setButton(0, dataRecieved[1] & C_DOWN_MASK);
     usrJoystick.setButton(1, dataRecieved[1] & C_RIGHT_MASK);
     usrJoystick.setButton(2, dataRecieved[1] & C_LEFT_MASK);
@@ -67,7 +78,11 @@ void i2cRecieveHandler(int numBytes){
     usrJoystick.setButton(8, dataRecieved[0] & START_MASK);
     usrJoystick.setButton(9, dataRecieved[0] & Z_MASK);
     
-    
+    usrJoystick.setXAxisRange(-128, 127);
+    usrJoystick.setYAxisRange(-128, 127);
+    usrJoystick.setXAxis(joyValX);
+    usrJoystick.setYAxis(joyValY);
+ 
 }
-//bool state = false;
+
 void loop(){}
